626c0aaa46d9d3795e233fc0745db1e167ec7c30
Diego Herrera <608492+diegoh@users.noreply.github.com>
2019-08-03 13:43:48 +0100

    Add healthcheck handler

--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
 const logger = require('@diegoh/logger');
-const startServer = require('@diegoh/webserver');
-const config = require('./config');
-const router = require('./router');
+const { name, version } = require('../../../package.json');
 
-try {
-  module.exports = startServer(
-    { port: config.server.port },
-    router.routes(),
-    router.allowedMethods()
-  );
-} catch (error) {
-  logger.error(error);
-  throw error;
-}
+const logCall = () =>
+  logger.info({
+    code: `${name}:HEALTH:0001`,
+    message: 'Called health endpoint'
+  });
+
+const logOk = health =>
+  logger.info({
+    code: `${name}:HEALTH:0002`,
+    message: 'Health OK',
+    health
+  });
+
+module.exports = async (ctx, next) => {
+  logCall();
+
+  const health = {
+    name,
+    version,
+    success: true
+  };
+
+  ctx.body = health;
+  ctx.status = 200;
+
+  logOk(health);
+  await next();
+};
--- src/router.js
+++
@@ -1,8 +0,0 @@
-const Router = require('@koa/router');
-const router = new Router();
-const health = require('./health/handlers');
-
-router.use(health.routes());
-router.use(health.allowedMethods());
-
-module.exports = router;
